fc2b1fa7ddfe80fce3f17bb1a32f43d493cd7b6a
pal2097 <pal2097@gmail.com>
2024-01-07 12:40:26 +0800

    feature: Update文章

--- app.config.ts
+++ app.config.ts
@@ -11,7 +11,7 @@ export default defineAppConfig({
       branch: 'main',
       repo: 'docus',
       owner: 'nuxt-themes',
-      edit: true
+      edit: false
     },
     aside: {
       level: 0,
